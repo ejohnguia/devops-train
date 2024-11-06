3c49cd13add0a70fef50bdc3e0220e2c18e1463d
ejohnguia <ezrajohn.guia@gmail.com>
2024-11-06 14:45:44 -0700

    feat: create os disk

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,18 @@ locals {
     location  = local.location
     parent_id = module.resource_group.id
   }
+
+  nic = {
+    name      = "my-nic"
+    location  = local.location
+    parent_id = module.resource_group.id
+  }
+
+  os_disk = {
+    name      = "my-os-disk"
+    location  = local.location
+    parent_id = module.resource_group.id
+  }
 }
 
 
@@ -27,4 +39,11 @@ module "network" {
   source = "./modules/network"
   vnet   = local.vnet
   nsg    = local.nsg
+  # nic    = local.nic
+}
+
+module "os_disk" {
+  source          = "./modules/storage"
+  os_disk         = local.os_disk
+  subscription_id = var.subscription_id
 }
--- terraform/modules/storage/variables.tf
+++ terraform/modules/storage/variables.tf
@@ -1,7 +1,13 @@
-variable "rg" {
-  description = "Resource Group for the deployment"
+variable "subscription_id" {
+  description = "Subscription ID for the deployment"
+  type        = string
+}
+
+variable "os_disk" {
+  description = "OS Disk for the deployment"
   type = object({
-    name     = string
-    location = string
+    name      = string
+    location  = string
+    parent_id = string
   })
 }
