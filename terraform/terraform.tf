b0d6e6dcbc0b65eae2fd43c5ccf343bf88d58510
ejohnguia <ezrajohn.guia@gmail.com>
2025-02-09 16:09:43 -0700

    fix: section 7 - 16 - upgrade version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,12 +2,12 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      #   version = "~> 3.0"
     }
     random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
+      source = "hashicorp/random"
+      #   version = "3.1.0"
     }
   }
 }
