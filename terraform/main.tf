4be7b3c42bfb7bb81fccc918715c9552ece88db7
ejohnguia <ezrajohn.guia@gmail.com>
2025-02-12 19:47:24 -0700

    section 7 - 20

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
+locals {
+  team        = "api_mgmt_dev"
+  application = "corp_api"
+  server_name = "ec2-${var.environment}-${var.variables_sub_az}"
+}
+
 #Define the VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
@@ -140,7 +146,9 @@ resource "aws_instance" "web_server" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
   tags = {
-    Name = "Ubuntu EC2 Server"
+    Name   = local.server_name
+    Owners = local.team
+    App    = local.application
   }
 }
 
