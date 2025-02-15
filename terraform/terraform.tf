f7ec53b92a2f6f9ffe89ac9ba05fe75003c6301b
ejohnguia <ezrajohn.guia@gmail.com>
2025-02-15 15:41:34 -0700

    section 7 - 27

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  # required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       #   version = "~> 3.0"
     }
+    http = {
+      source = "hashicorp/http"
+    }
     random = {
       source = "hashicorp/random"
-      #   version = "3.1.0"
+    }
+    local = {
+      source = "hashicorp/local"
     }
   }
 }
