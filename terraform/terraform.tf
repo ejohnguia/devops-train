026ece3e9f8a172ec098a3ad834f42bb5da35e79
ejohnguia <ezrajohn.guia@gmail.com>
2025-02-15 15:54:10 -0700

    fix: add version to providers

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,20 +2,24 @@ terraform {
   # required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      #   version = "~> 3.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
+      version = "~> 3.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
 }
