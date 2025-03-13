e6ca08c351baf9d71e68cfb7b975db581d0f3529
ejohnguia <ezrajohn.guia@gmail.com>
2025-03-12 19:02:28 -0600

    section 10 - 49

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,21 +137,21 @@ resource "aws_nat_gateway" "nat_gateway" {
 }
 
 # # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   owners = ["099720109477"]
-# }
+  owners = ["099720109477"]
+}
 
 # Terraform Data Block - Lookup Ubuntu 22.04
 # data "aws_ami" "ubuntu" {
@@ -526,3 +526,13 @@ resource "aws_key_pair" "generated" {
 #     path = "mystate/terraform.tfstate"
 #   }
 # }
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server_2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_2"].id
+  tags = {
+    Name = "Web EC2 Server"
+  }
+}
