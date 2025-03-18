f86e3c52e976b5e5310826924335f165fa0a51d5
ejohnguia <ezrajohn.guia@gmail.com>
2025-03-17 21:25:21 -0600

    section 10 - 64

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,28 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
+# data "aws_s3_bucket" "data_bucket" {
+#   bucket = "my-tf-test-bucket"
+# }
+
+# resource "aws_iam_policy" "policy" {
+#   name        = "data_bucket_policy"
+#   description = "Allow access to my bucket"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Action" : [
+#           "s3:Get*",
+#           "s3:List*"
+#         ],
+#         "Resource" : "${data.aws_s3_bucket.data_bucket.arn}"
+#       }
+#     ]
+#   })
+# }
+
 locals {
   team        = "api_mgmt_dev"
   application = "corp_api"
@@ -545,3 +567,15 @@ output "phone_number" {
   value     = var.phone_number
   sensitive = true
 }
+
+# output "data-bucket-arn" {
+#   value = data.aws_s3_bucket.data_bucket.arn
+# }
+
+# output "data-bucket-domain-name" {
+#   value = data.aws_s3_bucket.data_bucket.bucket_domain_name
+# }
+
+# output "data-bucket-region" {
+#   value = "The ${data.aws_s3_bucket.data_bucket.id} bucket is located in ${data.aws_s3_bucket.data_bucket.region}"
+# }
