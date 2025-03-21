6b66656703b7ace723c814a40c127511183072ca
ejohnguia <ezrajohn.guia@gmail.com>
2025-03-20 19:32:06 -0600

    section 10 - 66

--- terraform/main.tf
+++ terraform/main.tf
@@ -304,6 +304,22 @@ resource "aws_key_pair" "generated" {
 }
 
 # # Security Groups
+resource "aws_security_group" "main" {
+  name   = "core-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = var.web_ingress
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+}
+
 resource "aws_security_group" "ingress-ssh" {
   name   = "allow-all-ssh"
   vpc_id = aws_vpc.vpc.id
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -93,6 +93,31 @@ variable "env" {
   }
 }
 
+variable "web_ingress" {
+  type = map(object(
+    {
+      description = string
+      port        = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }
+  ))
+  default = {
+    "80" = {
+      description = "Port 80"
+      port        = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    "443" = {
+      description = "Port 443"
+      port        = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
 variable "num_1" {
   type        = number
   description = "Numbers for function labs"
